6c92b854bc0597f8d8059153a0080128317ec3ac
Scott <slangeberg@gmail.com>
2012-10-30 23:05:33 -0500

    Added / updated function and scope koans

---
+++ koans/AboutFunctions.js
@@ -0,0 +1,42 @@
+
+koansuite( "About Functions", function() {
+
+    // Replace any instance of __ that you find
+
+    koan( "Outer and inner scopes", function(){
+
+        var outer = function() {
+
+            var george = "monkey";
+
+            var inner = function() {
+                george = "fish"
+                return george;
+            }
+            return inner;
+        }
+
+        assert( (outer.george === undefined) === __, "George?")
+
+        assert( outer.hasOwnProperty("george") === __, "George, that you?")
+
+        assert( (outer().george === __) === true, "George, where are you?")
+
+        assert( outer().hasOwnProperty("george") === __, "George, are you in there?")
+
+        assert( (typeof(outer()) === "function") === __, "What are you?")
+
+
+        var inner = outer();
+
+        assert( (inner.george === undefined) === __, "George?")
+
+        assert( inner.hasOwnProperty("george") === __, "George, that you?")
+
+        assert( inner().hasOwnProperty("george") === __, "Did I find you George?")
+
+        assert( (typeof(inner()) === __ ) === true, "What's inside?")
+
+        assert( inner() === __, "What happened to you?")
+    });
+} );
