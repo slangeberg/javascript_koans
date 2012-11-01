6eee42430c518d61006589f02a6ff6f23416a189
Scott <slangeberg@gmail.com>
2012-10-31 21:56:46 -0500

    Renamed Functions to FunctionScope
    Started adapting AboutFunctions

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -1,42 +1,115 @@
+koansuite("About Functions", function() {
 
-koansuite( "About Functions", function() {
+    koan("Function Declarations", function() {
 
-    // Replace any instance of __ that you find
-
-    koan( "Outer and inner scopes", function(){
-
-        var outer = function() {
-
-            var george = "monkey";
-
-            var inner = function() {
-                george = "fish"
-                return george;
-            }
-            return inner;
+        function add(a, b) {
+            return a + b;
         }
 
-        assert( (outer.george === undefined) === __, "George?")
+        var subtract = function( c, d ) {
+            return c - d;
+        }
 
-        assert( outer.hasOwnProperty("george") === __, "George, that you?")
+        assert( add(1, 2) === 3, "How much?");
 
-        assert( (outer().george === __) === true, "George, where are you?")
-
-        assert( outer().hasOwnProperty("george") === __, "George, are you in there?")
-
-        assert( (typeof(outer()) === "function") === __, "What are you?")
-
-
-        var inner = outer();
-
-        assert( (inner.george === undefined) === __, "George?")
-
-        assert( inner.hasOwnProperty("george") === __, "George, that you?")
-
-        assert( inner().hasOwnProperty("george") === __, "Did I find you George?")
-
-        assert( (typeof(inner()) === __ ) === true, "What's inside?")
-
-        assert( inner() === __, "What happened to you?")
+        assert( subtract(3, 4) === -1, "Variables can reference functions");
     });
-} );
+
+    koan("should know internal variables override outer variables", function () {
+        var message = "Outer";
+
+        function getMessage() {
+            return message;
+        }
+
+        function overrideMessage() {
+            var message = "Inner";
+            return message;
+        }
+
+        expect(getMessage()).toBe(FILL_ME_IN);
+        expect(overrideMessage()).toBe(FILL_ME_IN);
+        expect(message).toBe(FILL_ME_IN);
+    });
+
+    koan("should have lexical scoping", function () {
+        var variable = "top-level";
+        function parentfunction() {
+            var variable = "local";
+            function childfunction() {
+                return variable;
+            }
+            return childfunction();
+        }
+        expect(parentfunction()).toBe(FILL_ME_IN);
+    });
+
+    koan("should use lexical scoping to synthesise functions", function () {
+
+        function makeIncreaseByFunction(increaseByAmount) {
+            var increaseByFunction = function increaseBy(numberToIncrease) {
+                return numberToIncrease + increaseByAmount;
+            };
+            return increaseByFunction;
+        }
+
+        var increaseBy3 = makeIncreaseByFunction(3);
+        var increaseBy5 = makeIncreaseByFunction(5);
+
+        expect(increaseBy3(10) + increaseBy5(10)).toBe(FILL_ME_IN);
+    });
+
+    koan("should allow extra function arguments", function () {
+
+        function returnFirstArg(firstArg) {
+            return firstArg;
+        }
+
+        expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+
+        function returnSecondArg(firstArg, secondArg) {
+            return secondArg;
+        }
+
+        expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+
+        function returnAllArgs() {
+            var argsArray = [];
+            for (var i = 0; i < arguments.length; i += 1) {
+                argsArray.push(arguments[i]);
+            }
+            return argsArray.join(",");
+        }
+
+        expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+    });
+
+    koan("should pass functions as values", function () {
+
+        var appendRules = function (name) {
+            return name + " rules!";
+        };
+
+        var appendDoubleRules = function (name) {
+            return name + " totally rules!";
+        };
+
+        var praiseSinger = { givePraise: appendRules };
+        expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+
+        praiseSinger.givePraise = appendDoubleRules;
+        expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
+
+    });
+
+    koan("should use function body as a string", function () {
+        var add = new Function("a", "b", "return a + b;");
+        expect(add(1, 2)).toBe(FILL_ME_IN);
+
+        var multiply = function (a, b) {
+            //An internal comment
+            return a * b;
+        };
+        expect(multiply.toString()).toBe(FILL_ME_IN);
+    });
+});
